d3884f884857cde88215cc7229f8442f9384bc88
Xuanyi Fu <xfu11@jhu.edu>
2021-10-26 14:03:03 +0800

    implement set with unordered lists

--- sets.rkt
+++ sets.rkt
@@ -20,3 +20,18 @@
   (element-of-set? 1 (adjoin-set 1 '(1)))
   (not (element-of-set? 0 (adjoin-set 1 '()))))
 
+(define (set-intersection s1 s2)
+  (cond ((or (null? s1) (null? s2)) '())
+        (else (let ((x1 (car s1))
+              (next-intersection (set-intersection (cdr s1) s2)))
+          (cond ((element-of-set? x1 s2) (cons x1 next-intersection))
+                (else next-intersection))))))
+
+(test "set-intersection"
+  (equal? (set-intersection '() '()) '())
+  (equal? (set-intersection '(1) '()) '())
+  (equal? (set-intersection '(1) '(1)) '(1))
+  (equal? (set-intersection '(1 2) '(4 5 2)) '(2))
+  (equal? (set-intersection '(1 2 3) '(4 5 6)) '())
+  )
+
